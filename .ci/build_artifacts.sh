da7ae633a06920b55e05ae7498585fbccc3d065e
Dmitry Maslennikov <mrdaimor@gmail.com>
2021-11-23 13:57:14 +0300

    remove xml

--- .ci/build_artifacts.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-ARTIFACT=`pwd`/out/zUtils.FileBinaryTar.xml
-
-iris session $ISC_PACKAGE_INSTANCENAME "##class(%SYSTEM.OBJ).Export(\"%zUtils.FileBinaryTar.cls\",\"$ARTIFACT\",\"/diffexport/exportversion=2017.1\")"
-
-exit 1
