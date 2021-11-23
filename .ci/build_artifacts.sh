b3b4f7aa8b96150d347760449ffa6f52d3bee35b
Dmitry Maslennikov <mrdaimor@gmail.com>
2021-11-23 13:36:43 +0300

    fix xml build

--- .ci/build_artifacts.sh
+++ .ci/build_artifacts.sh
@@ -1,18 +1,7 @@
 #!/bin/sh
 
-set -x 
-
 ARTIFACT=`pwd`/out/zUtils.FileBinaryTar.xml
-
-mkdir -p $( dirname $ARTIFACT ) 
-
-iris start $ISC_PACKAGE_INSTANCENAME \
 
 iris session $ISC_PACKAGE_INSTANCENAME "##class(%SYSTEM.OBJ).Export(\"%zUtils.FileBinaryTar.cls\",\"$ARTIFACT\",\"/diffexport/exportversion=2017.1\")"
 
-iris stop $ISC_PACKAGE_INSTANCENAME quietly
-
-if [ ! -f "$ARTIFACT" ]
-then
-  exit 1
-fi
+exit 1
